07734f46b1eaa379c630225c538d698008819cbf
Boneman87 <111004527+Boneman87@users.noreply.github.com>
2023-06-03 16:48:58 +0100

    Update lambda_sns_notification.py
    updated with student's inputs such as SNS topic ARN

--- code/part2/lambda_sns_notification.py
+++ code/part2/lambda_sns_notification.py
@@ -16,9 +16,9 @@ import json
 
 # ======================== EDIT THIS SECTION ========================================
 # Insert your SNS topic ARN below
-sns_arn = "arn:aws:sns:your:topic:here" 
+sns_arn = "arn:aws:sns:eu-west-1:583354510353:DEOLUAKI-streaming-sns-topic" 
 # Insert your full name and surname for marking purposes
-student_name = "Dora Explorer" 
+student_name = "Oluwasaanu Akinniyi" 
 # ===================================================================================
 
 def lambda_handler(event, context):
